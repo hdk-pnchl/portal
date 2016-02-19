adc49c469801394f3afb21a275dcd3d943301a0d
Hardik Panchal <hardik.panchal@boku.com>
2016-02-19 22:12:25 +0530

    fixed radio buttons

---
+++ src/main/java/com/deaakasheeshah/business/util/CommonUtil.java
@@ -0,0 +1,35 @@
+package com.deaakasheeshah.business.util;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class CommonUtil {
+	protected static AtomicReference<Long> currentTime = new AtomicReference<>(System.currentTimeMillis());
+
+	/**
+	 * will fetch unique long value based on current time. to avoid duplicates,
+	 * AtomicReference is used which provides volatile behaviour for counter.
+	 */
+	public static Long nextRegNo() {
+		Long prev;
+		Long next = System.currentTimeMillis();
+		do {
+			prev = currentTime.get();
+			/*
+			 * we need this, as its likely program executes so fast that, prev
+			 * and next both might have the same value.
+			 */
+			next = next > prev ? next : prev + 1;
+		}
+		/*
+		 * each time we fetch 1 new ID, we update its value in currentTime. And
+		 * we take reference of it to make sure, we are threadsafe.
+		 * 
+		 * compareAndSet will provide a check against stale values. i.e. while
+		 * updating currentTime's values, if existing copied value of
+		 * currentTime isnt same as the current value of currentTime, some other
+		 * thread already have taken the nextID.
+		 */
+		while (!currentTime.compareAndSet(prev, next));
+		return next;
+	}
+}
---
+++ src/test/java/org/hibernate/MainApp.java
@@ -0,0 +1,12 @@
+package org.hibernate;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+
+
+    }//main
+
+}
